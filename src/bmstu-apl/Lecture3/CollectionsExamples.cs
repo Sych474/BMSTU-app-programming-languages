08f4090809c6bc2b464db0db7368634e5f4cbd98
Svyatoslav Sychev <sychev@mindbox.ru>
2022-09-14 22:58:43 +0300

    add lecture and CollectionsExamples.cs

---
+++ src/bmstu-apl/Lecture3/CollectionsExamples.cs
@@ -0,0 +1,14 @@
+﻿namespace Lecture3;
+
+public static class CollectionsExampleCall
+{
+    public static void ForeachCall()
+    {
+        var emails = new string[] { "petya@kek.ru", "sasha@rar.ru", "masha@bst.ru" };
+
+        foreach (var email in emails)
+        {
+            Console.WriteLine(email);
+        }
+    }
+}
