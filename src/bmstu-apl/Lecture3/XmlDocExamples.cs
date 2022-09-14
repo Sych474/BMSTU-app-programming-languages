1f8720962e4126ae5e6fddc5df9fdd0dab6d92cb
Svyatoslav Sychev <sychev@mindbox.ru>
2022-09-14 22:27:50 +0300

    Add XmlDocExamples.cs

---
+++ src/bmstu-apl/Lecture3/XmlDocExamples.cs
@@ -0,0 +1,21 @@
+﻿namespace Lecture3;
+
+/// <summary>
+/// Interface for simple repository that allow save entities
+/// </summary>
+public interface ISaveRepository<T> where T : notnull
+{
+    /// <summary>
+    /// Method for saving new entity to repository
+    /// </summary>
+    /// <param name="item">New entity for save. </param>
+    /// <returns>Generated Id of new entity. </returns>
+    /// <exception cref="ArgumentException"> If argument is already in repository. </exception>
+    Guid Save(T item);
+}
+
+/// <inheritdoc cref="ISaveRepository{T}"/>
+public class SaveRepositoryStub<T> : ISaveRepository<T> where T : notnull
+{
+    public Guid Save(T item) => Guid.NewGuid();
+}
