18f140f927285aa0682c93c6eab169e178aa228f
Svyatoslav Sychev <sychev@mindbox.ru>
2022-09-14 22:12:29 +0300

    Add exception examples for lecture 3

---
+++ src/bmstu-apl/Lecture3/ExceptionsExamples.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Serialization;
+
+namespace Lecture3;
+
+public class CustomException : Exception
+{
+    public CustomException() { }
+    public CustomException(string? message) : base(message) { }
+    public CustomException(string? message, Exception? innerException) : base(message, innerException) { }
+}
+
+
+    
+public partial class CallExample
+{
+    private static void Foo() => throw new OutOfMemoryException("MyCustomMessage");
+    private static void Bar() => throw new ArgumentException();
+    
+    public static void ExceptionsCatchExample()
+    {
+        try
+        {
+            Foo();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    
+    public static void FullCatchBlockExample()
+    {
+        try
+        {
+            Foo();
+            Console.WriteLine("No Errors in Foo");
+        }
+        catch (OutOfMemoryException e) when(e.Message.Contains("MyCustomMessage"))
+        {
+            Console.WriteLine("Filtered OutOfMemoryException was thrown");
+        }
+        catch (OutOfMemoryException e)
+        {
+            Console.WriteLine("Other OutOfMemoryException was thrown");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            Console.WriteLine("Finally be called anyway.");
+        } 
+    }
+}
