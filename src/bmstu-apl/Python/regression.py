a7cf581e5e93259c0e67048c827c18771ae3db15
Svyatoslav Sychev <sychev@mindbox.ru>
2022-12-09 02:05:00 +0300

    update

--- src/bmstu-apl/Python/regression.py
+++ src/bmstu-apl/Python/regression.py
@@ -4,7 +4,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the diabetes dataset
 diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
